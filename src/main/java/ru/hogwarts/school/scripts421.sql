0566c7bbc848c53cc068c36de055d93f4d02aabf
Gorbun0v1 <joongleboo01@gmail.com>
2023-11-29 21:40:16 +0300

    4.2

---
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -0,0 +1,18 @@
+alter table student
+    add constraint age_constraint check ( age > 16 );
+
+alter table student
+    alter column age set default 20;
+
+alter table student
+    alter column name set not null;
+
+alter table student
+    add unique (name);
+
+alter table faculty
+    add constraint color_name_unique unique (color, name);
+
+
+alter table student
+drop constraint age_constraint;
